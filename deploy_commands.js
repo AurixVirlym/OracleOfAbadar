5427e49748cfc82792036f9f16b2f28a8376748a
Danni <danielt9001@gmail.com>
2022-11-26 12:39:59 +0000

    Ready for Beta Patch

--- deploy_commands.js
+++ deploy_commands.js
@@ -11,34 +11,56 @@ const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 
+	new SlashCommandBuilder().setName('oraclehelp')
+	.setDescription('Gives information on Oracle of Abadar Commands, select "all" for generic overview.')
+	.addStringOption(option => option.setName('command').setDescription('Name of command you want information on.').setRequired(false).addChoices(
+		{ name: 'all', value: 'all' },
+		{ name: 'addplayer', value: 'addplayer' },
+		{ name: 'infoplayer', value: 'infoplayer' },
+		{ name: 'infochar', value: 'infochar' },
+		{ name: 'inforeport', value: 'inforeport' },
+		{ name: 'purchase ', value: 'purchase' },
+		{ name: 'changeslots', value: 'changeslots' },
+		{ name: 'changestatus', value: 'changestatus' },
+		{ name: 'changereportdescription', value: 'changereportdescription' },
+		{ name: 'addapproval', value: 'addapproval' },
+		{ name: 'newchar', value: 'newchar' },
+		{ name: 'addxp', value: 'addxp' },
+		{ name: 'recalculatecharacter', value: 'recalculatecharacter' },
+		{ name: 'newsessionreport', value: 'newsessionreport' },
+		{ name: 'addchartosr', value: 'addchartosr' },
+		{ name: 'assignreporttochar', value: 'assignreporttochar' },
+		{ name: 'publishsessionreport', value: 'publishsessionreport' },
+		)),
+
     new SlashCommandBuilder().setName('addplayer')
 	.setDescription('Adds player to database')
-	.addStringOption(option => option.setName('addplayerinput').setDescription('Enter a name').setRequired(true)),
+	.addStringOption(option => option.setName('mention').setDescription('Player discord @mention').setRequired(true)),
 	
 
 	new SlashCommandBuilder().setName('infoplayer')
 	.setDescription('Gives info on a existing player.')
-	.addStringOption(option => option.setName('infoplayerinput').setDescription('Enter a name')),
+	.addStringOption(option => option.setName('mention').setDescription('Player discord @mention')),
 
 
 	new SlashCommandBuilder().setName('assignreporttochar')
 	.setDescription('Gives a you own unassigned report to one of your characters.')
 	.addStringOption(option => option.setName('reportname').setDescription('Enter the unassigned report name which you own.').setRequired(true))
-	.addStringOption(option => option.setName('charnameinput').setDescription('Character Name').setMinLength(1).setMaxLength(30).setRequired(true)),
+	.addStringOption(option => option.setName('character').setDescription('Character Name, captials matter.').setMinLength(1).setMaxLength(30).setRequired(true)),
 
 
 	new SlashCommandBuilder().setName('newchar')
 	.setDescription('Adds a new character to a player.')
-	.addStringOption(option => option.setName('mentioninput').setDescription('Player discord @mention').setRequired(true))
-	.addStringOption(option => option.setName('charinput').setDescription('Name of the new character').setMinLength(1).setMaxLength(30).setRequired(true))
+	.addStringOption(option => option.setName('mention').setDescription('Player discord @mention').setRequired(true))
+	.addStringOption(option => option.setName('character').setDescription('Name of the new character, captials matter').setMinLength(1).setMaxLength(30).setRequired(true))
 	.addNumberOption(option => option.setName('startlevel').setDescription('Number of character slots').setRequired(true).addChoices(
-		{ name: 'First', value: 1 },
-		{ name: 'Second', value: 2 },)),
+		{ name: '1', value: 1 },
+		{ name: '2', value: 2 },)),
 
 
 	new SlashCommandBuilder().setName('changeslots')
 	.setDescription('Set a players character slot limit to the given value.')
-	.addStringOption(option => option.setName('mentioninput').setDescription('Player discord @mention').setRequired(true))
+	.addStringOption(option => option.setName('mention').setDescription('Player discord @mention').setRequired(true))
 	.addNumberOption(option => option.setName('slots').setDescription('Number of character slots').setMinValue(0).setMaxValue(666).setRequired(true)),
 	
 	new SlashCommandBuilder().setName('changestatus')
@@ -52,12 +74,21 @@ const commands = [
 		{ name: 'M.I.A.', value: 'M.I.A.' },
 		{ name: 'Retired', value: 'Retired' },
 		{ name: 'Shadowrealm (Banned)', value: 'Shadowrealm (Banned)' },))
-	.addStringOption(option => option.setName('character').setDescription('Character Name'))
+	.addStringOption(option => option.setName('character').setDescription('Character name, captials matter'))
 	.addStringOption(option => option.setName('mention').setDescription('Player discord @mention, use for changing the status on characters of a player.')),
 
-	new SlashCommandBuilder().setName('infochar')
+	new SlashCommandBuilder().setName('infocharacter')
 	.setDescription('Gives information on a given character of a player')
-	.addStringOption(option => option.setName('infocharinput').setDescription('Character Name').setMinLength(1).setMaxLength(30).setRequired(true)),
+	.addStringOption(option => option.setName('character').setDescription('Character Name').setMinLength(1).setMaxLength(30).setRequired(true)),
+
+	new SlashCommandBuilder().setName('addxp')
+	.setDescription('Adds/Subtracts XP to a character, it is not counted in the player xp total.')
+	.addStringOption(option => option.setName('character').setDescription('Character Name').setMinLength(1).setMaxLength(30).setRequired(true))
+	.addNumberOption(option => option.setName('xp').setDescription('The amount of XP to change').setMinValue(-1000).setMaxValue(1000).setRequired(true)),
+
+	new SlashCommandBuilder().setName('recalculatecharacter')
+	.setDescription('Forces a recalculation of a characters reports.')
+	.addStringOption(option => option.setName('character').setDescription('Character Name').setMinLength(1).setMaxLength(30).setRequired(true)),
 	
 	new SlashCommandBuilder().setName('inforeport')
 	.setDescription('gives information on a given report.')
@@ -72,6 +103,11 @@ const commands = [
 	.setDescription('Publishes a report, making it uneditable and hands out the rewards to the listed characters and GMs.')
 	.addStringOption(option => option.setName('reportname').setDescription('Report name, must be unpublished').setMinLength(1).setMaxLength(60).setRequired(true)),
 
+	new SlashCommandBuilder().setName('changereportdescription')
+	.setDescription("Changes a report's description, must be unpublished.")
+	.addStringOption(option => option.setName('reportname').setDescription('Report name, must be unpublished').setMinLength(1).setMaxLength(60).setRequired(true))
+	.addStringOption(option => option.setName('reportdescription').setDescription('New report description, must be under 3000 characters').setMinLength(1).setMaxLength(3000).setRequired(true)),
+
 	new SlashCommandBuilder().setName('addchartosr')
 	.setDescription('Adds characters to an unpublished report, up to 8 at a time.')
 	.addStringOption(option => option.setName('reportname').setDescription('Exact report name you wish to add the characters to, must be unpublished.').setMinLength(1).setMaxLength(60).setRequired(true))
@@ -85,10 +121,15 @@ const commands = [
 	.addStringOption(option => option.setName('character-8').setDescription('Exact name of the character you wish to add to the report.').setMinLength(1).setMaxLength(30)),
 	
 	new SlashCommandBuilder().setName('purchase')
-	.setDescription('Purcahes or sells an item on a character you own. Give a negative value to sell an item.')
+	.setDescription('Purchases or sells an item on a character you own. Give a negative value to sell an item.')
 	.addStringOption(option => option.setName('character').setDescription('Character Name').setMinLength(1).setMaxLength(30).setRequired(true))
 	.addStringOption(option => option.setName('item').setDescription('Name of item(s) purchased or sold.').setMinLength(1).setMaxLength(80).setRequired(true))
 	.addNumberOption(option => option.setName('gp').setDescription('Gold spent/gained').setMinValue(-3000).setMaxValue(3000).setRequired(true)),
+
+	new SlashCommandBuilder().setName('addapproval')
+	.setDescription("Adds an entry to the a character's approval log. Staff Only.")
+	.addStringOption(option => option.setName('character').setDescription('Character Name').setMinLength(1).setMaxLength(30).setRequired(true))
+	.addStringOption(option => option.setName('approval').setDescription('approval entry to add.').setMinLength(1).setMaxLength(120).setRequired(true)),
 	
 ]
 .map(command => command.toJSON());
