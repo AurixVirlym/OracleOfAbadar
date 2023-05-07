9cffebd676a7baed870c7211e3ebac7f7c6d9762
Danni <danielt9001@gmail.com>
2023-05-07 17:28:56 +0100

    07/05/2023 -2=

---
+++ commands/infocharactershown.js
@@ -0,0 +1,33 @@
+
+const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const {
+} = require('../constants.js');
+
+
+
+module.exports = {
+	data: new ContextMenuCommandBuilder()
+	.setName('infocharactershown')
+	.setType(ApplicationCommandType.User),
+
+	async execute(interaction,client) {
+        
+        
+        const command = interaction.client.commands.get("infocharacter");
+
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+			await command.execute(interaction,client,interaction.targetUser,false);
+
+		} catch (error) {
+			console.error(`Error executing ${interaction.commandName}`);
+			console.error(error);
+		}
+
+		return
+    }
+}
